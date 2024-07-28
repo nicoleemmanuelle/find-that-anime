1c254de8ebeff0826c37799880d8e86f560e7b7a
nicoleemmanuelle <nicole.dona.24@gmail.com>
2024-07-28 14:44:07 +0800

    Deployed website

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
                 <a className="navbar-brand" href="/">
                         <img src="src/assets/find-that-anime-logo.png" alt=""></img>
                 </a>
-                <img id="light-and-dark-mode" src="public/dark-mode.png" height="30" onClick={changeMode}></img>
+                <img id="light-and-dark-mode" src="src/assets/dark-mode.png" height="30" onClick={changeMode}></img>
                 {/* <div className="form-check form-switch mx-4">
                     <input className="form-check-input p-2 checkbox" type="checkbox" role="switch" id="light-and-dark-mode-checkbox" defaultChecked onClick={myFunction}></input>
                     <label for="light-and-dark-mode-checkbox" class="checkbox-label">
